8787c126fc69d7a6410b9689bc9dcb03277b22ee
Johnev3 <johne.arredondo@autonoma.edu.co>
2026-05-17 00:39:07 -0500

    actualizacion de clases de vista

--- juegobarkout/src/main/java/model/Entity.java
+++ juegobarkout/src/main/java/model/Entity.java
@@ -1,4 +1,30 @@
 package model;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
+
 public class Entity {
+    public Entity(){
+
+    }
+
+    /**
+     * Metodo para cargar las imagenes
+     * @param fileName nombre del archivo "imagen.jpg"...
+     * @return imagen
+     */
+    public static BufferedImage uploadImage(String fileName) {
+        try {
+            InputStream is = Entity.class.getResourceAsStream("/images/" + fileName);
+            if (is == null) {
+                System.err.println("Imagen no encontrada: " + fileName);
+                return null;
+            }
+            return ImageIO.read(is);
+        } catch (Exception e) {
+            System.err.println("Error cargando imagen: " + fileName);
+            return null;
+        }
+    }
 }
